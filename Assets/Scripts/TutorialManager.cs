27ea7b6cb7b400c04079d77bde2baa3941739d76
Soni520 <sophia.ninh@hotmail.com>
2026-02-18 16:45:31 +0100

    Tutorial and Fruitspwan fixed

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -4,6 +4,8 @@ using Meta.XR.BuildingBlocks;
 using Meta.XR.ImmersiveDebugger.UserInterface.Generic;
 using Unity.VisualScripting;
 using System.Security.Cryptography;
+using UnityEngine.UI;
+using Image = UnityEngine.UI.Image;
 
 public class TutorialManager : MonoBehaviour
 {
@@ -30,23 +32,24 @@ public class TutorialManager : MonoBehaviour
                     TutorialText.text = "\nUse the <b>Left Joystick</b> to <b>walk</b> around in the world and the <b>Right Joystick</b> to <b>look</b> around.\n\nIf you want to <b>pause</b> or get back to the <b>menu</b> press the <b>menu button</b> on your left controller.";
                     break;
                 case 1:
-                    SearchingObjectCanvas.transform.position = new Vector3(-1.093f, 0.552f, 3);
-                    SearchingObjectCanvas.GetComponentInChildren<Image>().Color = new Color(1, 1, 0, 1);
-                    TutorialCanvas.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(0.5f, -0.35f, 2.5f);
-                    TutorialCanvas.GetComponent<RectTransform>().sizeDelta = new Vector2(100, 70);
                     TutorialText.text = "\nThe hint in the <b>Upper Left Corner</b> shows you which <b>Fruit</b> to look for.";
+                    SearchingObjectCanvas.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(-1.093f, 0.552f, 3);
+                    SearchingObjectCanvas.GetComponentInChildren<Image>().color = new Color(1, 1, 0, 1);
+                    TutorialCanvas.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(0.5f, -0.35f, 3);
+                    TutorialCanvas.GetComponent<RectTransform>().sizeDelta = new Vector2(100, 70);
                     break;
                 case 2:
-                    SearchingObjectCanvas.GetComponentInChildren<Image>().Color = new Color(1, 1, 1, 200/255f);
-                    TutorialCanvas.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(0.8f, -0.2f, 2.5f);
+                    TutorialText.text = "\nIf you aren't getting closer after some time, <b>particles</b> will appear beneath your feet to <b>point the way</b>. These act as a guide toward the Fruit you are searching for. Once you begin walking in the <b>right direction</b>, the trail will <b>disappear</b>.";
+                    SearchingObjectCanvas.GetComponentInChildren<Image>().color = new Color(1, 1, 1, 200/255f);
+                    TutorialCanvas.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(0.6f, -0.2f, 3);
                     TutorialCanvas.GetComponent<RectTransform>().sizeDelta = new Vector2(150, 90);
                     SearchLogic.timeUntilHint = 0;
-                    TutorialText.text = "\nIf you aren't getting closer after some time, <b>particles</b> will appear beneath your feet to <b>point the way</b>. These act as a guide toward the Fruit you are searching for. Once you begin walking in the <b>right direction</b>, the trail will <b>disappear</b>.";
                     break;
                 case 3:
                     TutorialText.text = "Now try to find the fruit.";
+                    ContinueText.text = "Press B to hide this window.";
                     SearchLogic.timeUntilHint = 10;
-                    TutorialCanvas.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(0f, -0.2f, 2.5f);
+                    TutorialCanvas.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(0f, -0.2f, 3);
                     TutorialCanvas.GetComponent<RectTransform>().sizeDelta = new Vector2(135, 35);
                     break;
                 case 4:
@@ -56,11 +59,11 @@ public class TutorialManager : MonoBehaviour
                     break;
             }
         }
-        if (SearchLogic.showRays && TutorialStage >= 4)
+        if (SearchLogic.showRays && TutorialStage >= 3)
         {
             TutorialText.text = "Proceed to the <b>meditation</b> by aiming at the <b>fruit</b> and using the <b>trigger</b>.";
             ContinueText.text = "";
-            TutorialCanvas.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(-0.45f, 0.35f, 2.5f);
+            TutorialCanvas.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(-0.45f, 0.35f, 3);
             TutorialCanvas.GetComponent<RectTransform>().sizeDelta = new Vector2(105, 35);
             SearchingObjectCanvas.transform.position = new Vector3(-1.093f, 0.552f, -3);
         }
